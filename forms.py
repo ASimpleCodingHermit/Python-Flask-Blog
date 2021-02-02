671b8839740093616625e3e6cd5de255bf99bd77
Joseph Chu <jchucodes@gmail.com>
2021-02-02 06:33:42 -0500

    added login form with remember me functionality

--- forms.py
+++ forms.py
@@ -10,3 +10,12 @@ class RegistrationForm(FlaskForm):
 	confirm_password = PasswordField('Confirm Password', 
 		validators=[DataRequired(), EqualTo('PasswordField')])
 	submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+	username = StringField('Username', 
+		validators=[DataRequired(), Length(min=2, max=20) ])
+	email = StringField('Email', validators=[DataRequired(), Email() ])
+	password = PasswordField('Password', validators=[DataRequired()])
+	confirm_password = PasswordField('Confirm Password', 
+		validators=[DataRequired(), EqualTo('PasswordField')])
+	submit = SubmitField('Sign Up')
