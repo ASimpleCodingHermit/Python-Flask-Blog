201e166ddc0c95992228559c9e394832805f7216
Joseph Chu <jchucodes@gmail.com>
2021-02-02 23:12:52 -0500

    updated the Registration form code for validation. Removed the EqualTo section of code. Will update later with a better solution

--- python_blog.py
+++ python_blog.py
@@ -36,7 +36,7 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash('Account created!', 'success')
+        flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
