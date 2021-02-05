37f7758c7d17b8fdd275ee8c33daab9b5300e81d
Joseph Chu <jchucodes@gmail.com>
2021-02-05 10:53:56 -0500

    feature: Added Bcrypt Hashing feature for passwords

--- pythonblog/__init__.py
+++ pythonblog/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '1b259870436a179cf073be94cfb612f5'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
-
+bcrypt = Bcrypt(app)
 
 from pythonblog import routes
