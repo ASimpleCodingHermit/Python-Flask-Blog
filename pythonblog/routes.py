be5939e35195d17b4386419a42617a61d4ce5ffa
Joseph Chu <jchucodes@gmail.com>
2021-02-06 06:12:57 -0500

    Updated Bcrypt

--- pythonblog/routes.py
+++ pythonblog/routes.py
@@ -37,6 +37,7 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user=User(username=form.username.data, email=form.email.data, password=hashed_password)
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
